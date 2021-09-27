034b4e439e09b62dc57f80dc7916262d9a807033
GamKon <gamkon61@gmail.com>
2021-09-27 01:23:08 -0400

    added SG firewall for web-srv with dynamic blocks

--- network.tf
+++ network.tf
@@ -2,41 +2,33 @@ resource "aws_security_group" "ssh_http_https" {
   name = "allow_ssh_http_https_from_0"
   description = "Firewall for WebServer"
   # vpc_id = aws_vpc..id
-  ingress = [ 
-    {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      description = "inbound SSH port 22"
+
+  dynamic "ingress" {
+      for_each = ["80", "443"]
+      content {
+        cidr_blocks = [ "0.0.0.0/0" ]
+        description = "inbound from internet"
+        from_port = ingress.value
+        ipv6_cidr_blocks = [ "::/0" ]
+        prefix_list_ids = []
+        protocol = "tcp"
+        security_groups = []
+        self = false
+        to_port = ingress.value
+      }
+  }
+  ingress {
+      cidr_blocks = [ "67.69.76.206/32" ]
+      description = "inbound SSH port 22 from my IP"
       from_port = 22
-      ipv6_cidr_blocks = [ "::/0" ]
+      ipv6_cidr_blocks = []
       prefix_list_ids = []
       protocol = "tcp"
       security_groups = []
       self = false
       to_port = 22
-    },
-    {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      description = "inbound HTTP port 80"
-      from_port = 80
-      ipv6_cidr_blocks = [ "::/0" ]
-      prefix_list_ids = []
-      protocol = "tcp"
-      security_groups = []
-      self = false
-      to_port = 80
-    },
-    {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      description = "inbound HTTPs port 443"
-      from_port = 443
-      ipv6_cidr_blocks = [ "::/0" ]
-      prefix_list_ids = []
-      protocol = "tcp"
-      security_groups = []
-      self = false
-      to_port = 443
-    },
-  ]  
+    }
+  
   egress = [ {
     cidr_blocks = [ "0.0.0.0/0" ]
     description = "Outbound everything"
@@ -48,7 +40,7 @@ resource "aws_security_group" "ssh_http_https" {
     self = false
     to_port = 0
   } ]
-
+    
   tags = {
     Name = "allow_ssh_http_https"
   }
